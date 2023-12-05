25a99a648c74b9fb4537c6ef81e5fa6c3afbe5ba
wzp <1321994008@qq.com>
2023-12-06 01:03:32 +0800

    chore: 🔨 config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
+import process from 'node:process'
+import open from 'open'
 import { pwa } from './config/pwa'
 import { appDescription } from './constants/index'
 
 export default defineNuxtConfig({
+  devServer: {
+    host: '127.0.0.1',
+  },
+
   modules: [
     '@vueuse/nuxt',
     '@unocss/nuxt',
@@ -9,6 +15,21 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@vite-pwa/nuxt',
   ],
+
+  hooks: {
+    listen() {
+      if (process.env.NUXT_OPEN === 'false')
+        return
+
+      open(`http://127.0.0.1:${process.env.NUXT_PORT}`)
+    },
+  },
+
+  vite: {
+    server: {
+      proxy: {},
+    },
+  },
 
   experimental: {
     // when using generate, payload js assets included in sw precache manifest
@@ -22,19 +43,6 @@ export default defineNuxtConfig({
   css: [
     '@unocss/reset/tailwind.css',
   ],
-
-  nitro: {
-    esbuild: {
-      options: {
-        target: 'esnext',
-      },
-    },
-    prerender: {
-      crawlLinks: false,
-      routes: ['/'],
-      ignore: ['/hi'],
-    },
-  },
 
   app: {
     head: {
