011876e28ab956fb8ae72fdaf61958abc3731775
wzp <1321994008@qq.com>
2023-12-13 22:36:39 +0800

    chore: 🔨 config

--- app.vue
+++ app.vue
@@ -14,14 +14,11 @@ useHead({
 </template>
 
 <style>
-html, body , #__nuxt{
+html,
+body,
+#__nuxt {
   height: 100vh;
   margin: 0;
   padding: 0;
-}
-
-html.dark {
-  background: #222;
-  color: white;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,13 @@
-import process from 'node:process'
-import open from 'open'
 import { pwa } from './config/pwa'
 import { appDescription } from './constants/index'
 
 export default defineNuxtConfig({
-  devServer: {
-    host: '127.0.0.1',
-  },
-
   modules: [
     '@vueuse/nuxt',
     '@unocss/nuxt',
     '@pinia/nuxt',
-    '@nuxtjs/color-mode',
     '@vite-pwa/nuxt',
   ],
-
-  hooks: {
-    listen() {
-      if (process.env.NUXT_OPEN === 'false')
-        return
-
-      open(`http://127.0.0.1:${process.env.NUXT_PORT}`)
-    },
-  },
-
-  vite: {
-    server: {
-      proxy: {},
-    },
-  },
 
   experimental: {
     // when using generate, payload js assets included in sw precache manifest
@@ -43,6 +21,19 @@ export default defineNuxtConfig({
   css: [
     '@unocss/reset/tailwind.css',
   ],
+
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+    prerender: {
+      crawlLinks: false,
+      routes: ['/'],
+      ignore: ['/hi'],
+    },
+  },
 
   app: {
     head: {
