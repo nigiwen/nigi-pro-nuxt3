5634dbbb56c3c34d5c0769f59431a7a3dc8d3e06
wzp <1321994008@qq.com>
2023-12-24 18:59:19 +0800

    feat(layout): 🚀 login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,66 +1,55 @@
 <script setup lang='ts'>
+import { nanoid } from 'nanoid'
+
 definePageMeta({
   layout: false,
 })
 
+// 任何地方的滚轮都可以控制爆布区的滚动
 const el = ref<HTMLElement | null>(null)
 const { y } = useScroll(el)
 function onWheel(event: WheelEvent) {
   y.value += event.deltaY
 }
 
-function generateTwoColumnsArray(totalNumbers: number, columnCount: number, numbersPerColumn: number): number[][] {
-  const maxNumber = 200
-  const result: number[][] = []
-  for (let i = 0; i < columnCount; i++) {
-    const columnNumbers: number[] = []
+// 生成爆布区的id
+function generateArray(cols: number, rows: number): string[][] {
+  const result: string[][] = []
 
-    for (let j = 0; j < numbersPerColumn; j++) {
-      let randomNumber = Math.floor(Math.random() * maxNumber) + 1
+  for (let i = 0; i < rows; i++) {
+    const row: string[] = []
+    for (let j = 0; j < cols; j++)
+      row.push(nanoid())
 
-      // 如果已生成的数字总数达到上限，则重头开始
-      if (columnNumbers.length >= totalNumbers)
-        columnNumbers.length = 0
-
-      // 检查数字是否重复，如果重复则重新生成
-      while (columnNumbers.includes(randomNumber))
-        randomNumber = Math.floor(Math.random() * maxNumber) + 1
-
-      columnNumbers.push(randomNumber)
-    }
-    result.push(columnNumbers)
+    result.push(row)
   }
+
   return result
 }
-// 生成包含 2 列、每列包含 100 个不重复数字的数组
-const twoColumnsArray = useState('twoColumnsArray', () => generateTwoColumnsArray(100, 2, 200))
+const twoColumnsArray = useState('twoColumnsArray', () => generateArray(5, 2))
 
-const bgArr = reactive({
-  luxueqi: 3,
-  meidusha: 7,
-  qianrenxue: 10,
-  xiaowu: 8,
-  xiaowu2: 8,
-})
-function getRandomValue(obj: Record<string, number>): [string, number] {
-  const keys = Object.keys(obj)
-
-  if (keys.length === 0)
-    throw new Error('Object is empty.')
-
-  const randomKey = keys[Math.floor(Math.random() * keys.length)]
-  const randomNumber = Math.floor(Math.random() * obj[randomKey]) + 1
-
-  return [randomKey, randomNumber]
+interface FormState {
+  username: string
+  password: string
+  remember: boolean
 }
-const [randomKey, randomValue] = getRandomValue(bgArr)
 
-const dynamicBackgroundImageUrl = ref(`http://119.91.195.110:3001/static/image/beijing/${randomKey}/${randomValue}.jpg`)
+const formState = reactive<FormState>({
+  username: '',
+  password: '',
+  remember: true,
+})
+function onFinish(_values: any) {
+  // console.log('Success:', values)
+}
+
+function onFinishFailed(_errorInfo: any) {
+  // console.log('Failed:', errorInfo)
+}
 </script>
 
 <template>
-  <div bg="#F0F1F5" h-full w-full flex @wheel="onWheel">
-    <div w="45%" :style="{ backgroundImage: `url(${dynamicBackgroundImageUrl})` }" h-full bg-cover bg-top bg-no-repeat op="80" />
+  <div bg="#F0F1F5" h-full w-full flex class="bg-container" @wheel="onWheel">
     <div bg="#ffffff" m-auto h-150 w-250 flex flex-row overflow-hidden rounded-xl>
       <div ref="el" w="1/2" overflow-auto>
         <article>
@@ -68,15 +57,52 @@ const dynamicBackgroundImageUrl = ref(`http://119.91.195.110:3001/static/image/b
             <div grid="~ cols-2 gap-2">
               <div v-for="(twoColumns, k) in twoColumnsArray" :key="k" flex="~ col gap-2">
                 <div v-for="(col, j) in twoColumns" :key="j" overflow-hidden>
-                  <a-image :key="col" :src="`http://119.91.195.110:3001/static/image/thumbnail/${col}.jpg`" alt="" />
+                  <a-image :key="col" :src="`https://api.likepoems.com/img/pixiv/?v=${col}`" :alt="col" />
                 </div>
               </div>
             </div>
           </div>
         </article>
       </div>
-      <div w="1/2">
-        2
+      <div w="1/2" m-auto>
+        <a-form
+          w-80%
+          :model="formState"
+          name="basic"
+          :label-col="{ span: 8 }"
+          :wrapper-col="{ span: 16 }"
+          autocomplete="off"
+          @finish="onFinish"
+          @finish-failed="onFinishFailed"
+        >
+          <a-form-item
+            label="Username"
+            name="username"
+            :rules="[{ required: true, message: 'Please input your username!' }]"
+          >
+            <a-input v-model:value="formState.username" />
+          </a-form-item>
+
+          <a-form-item
+            label="Password"
+            name="password"
+            :rules="[{ required: true, message: 'Please input your password!' }]"
+          >
+            <a-input-password v-model:value="formState.password" />
+          </a-form-item>
+
+          <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
+            <a-checkbox v-model:checked="formState.remember">
+              Remember me
+            </a-checkbox>
+          </a-form-item>
+
+          <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+            <a-button type="primary" html-type="submit">
+              Submit
+            </a-button>
+          </a-form-item>
+        </a-form>
       </div>
     </div>
   </div>
@@ -85,5 +111,19 @@ const dynamicBackgroundImageUrl = ref(`http://119.91.195.110:3001/static/image/b
 <style scoped lang='scss'>
 ::-webkit-scrollbar {
   display: none;
+}
+:deep(.ant-image),
+:deep(.ant-image-img) {
+  height: 100%;
+}
+.bg-container:before {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  opacity: 0.3;
+  content: '';
+  position: fixed;
+  background: url(https://api.paugram.com/wallpaper/) center/cover;
 }
 </style>
